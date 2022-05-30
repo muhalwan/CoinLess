93167cb251e3a1b6372f33b402dc39e0dfbed85a
alwan <muhalwan12@gmail.com>
2022-05-31 04:02:29 +0700

    updated

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-exports.db = new Sequelize('coinless', 'uyuxgntwayyym696', 'WKvH2CvIrnpGOKGPYZLW', {
+exports.db = new Sequelize('bttogl4fpt9desspbkb0', 'uyuxgntwayyym696', 'WKvH2CvIrnpGOKGPYZLW', {
     host: 'bttogl4fpt9desspbkb0-mysql.services.clever-cloud.com',
     dialect: 'mysql'
 });
