9e6c411e8b161fb457b7b54744b954ab343db740
alwan <muhalwan12@gmail.com>
2022-06-07 12:56:12 +0700

    update

--- db/conn.js
+++ db/conn.js
@@ -1,11 +1,11 @@
 const {Client} = require('pg');
 
 const client = new Client({
-  host: 'ec2-52-73-155-171.compute-1.amazonaws.com',
-  user: 'yihisgdbwznouw',
+  host: 'coinlessdb.cfx4yrrlw52h.us-west-2.rds.amazonaws.com',
+  user: 'CoinLess_pg',
   port: 5432,
-  password: 'bdb0a511e8c3e1c41249793414bee4aff1b8b7db0996daf2634d0c82c0cdc0b1',
-  database: 'denfrlojvp0okh',
+  password: 'asdfg278',
+  database: 'postgres',
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
--- db/index.js
+++ db/index.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
 
 const config = {
-  host: 'ec2-52-73-155-171.compute-1.amazonaws.com',
-  user: 'yihisgdbwznouw',
-  password: 'bdb0a511e8c3e1c41249793414bee4aff1b8b7db0996daf2634d0c82c0cdc0b1',
-  database: 'denfrlojvp0okh',
+  host: 'coinlessdb.cfx4yrrlw52h.us-west-2.rds.amazonaws.com',
+  user: 'CoinLess_pg',
+  port: 5432,
+  password: 'asdfg278',
+  database: 'postgres',
 };
 
 const pool = mysql.createPool(config);
